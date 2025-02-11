82beda94d43daf91f4e3f05142dfeb8e4417bd19
Viktor Mirieiev <victor.mireyev@gmail.com>
2025-02-11 13:46:26 +0000

    In resource table in category dropdown, show only categories of offerings that...

--- src/marketplace/resources/list/CategoryFilter.tsx
+++ src/marketplace/resources/list/CategoryFilter.tsx
@@ -8,16 +8,29 @@ import {
 } from '@waldur/form/themed-select';
 import { translate } from '@waldur/i18n';
 import { categoryAutocomplete } from '@waldur/marketplace/common/autocompletes';
+import { Customer, Project } from '@waldur/workspace/types';
 
 export const CategoryFilter: React.FC<{
   reactSelectProps?: Partial<SelectProps>;
+  project?: Project;
+  customer?: Customer;
 }> = (props) => (
   <Field
     name="category"
     component={(fieldProps) => (
       <AsyncPaginate
         placeholder={translate('Select category...')}
-        loadOptions={categoryAutocomplete}
+        loadOptions={(query: string, prevOptions, { page }) =>
+          categoryAutocomplete(
+            query,
+            prevOptions,
+            { page },
+            {
+              resource_customer_uuid: props.customer?.uuid,
+              resource_project_uuid: props.project?.uuid,
+            },
+          )
+        }
         defaultOptions
         getOptionValue={(option) => option.uuid}
         getOptionLabel={(option) => option.title}
