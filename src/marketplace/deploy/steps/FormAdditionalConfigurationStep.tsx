800757235cd3518638a0d29a7c955fea7adcac73
Andrey Izman <andreyizman9@gmail.com>
2025-02-14 17:32:40 +0000

    [WAL-8200] Fix async select field of select_openstack_tenant and other similar selects and used marketplace-resources api endpoint to fetch the tenants and instances

--- src/marketplace/deploy/steps/FormAdditionalConfigurationStep.tsx
+++ src/marketplace/deploy/steps/FormAdditionalConfigurationStep.tsx
@@ -1,15 +1,21 @@
+import { useSelector } from 'react-redux';
+
 import { AccordionCard } from '@waldur/core/AccordionCard';
 import { OptionsForm } from '@waldur/marketplace/common/OptionsForm';
 
 import { FormStepProps } from '../types';
+import { formCustomerSelector } from '../utils';
 
-export const FormAdditionalConfigurationStep = (props: FormStepProps) => (
-  <AccordionCard
-    title={props.title}
-    id={props.id}
-    className="step-card"
-    defaultOpen
-  >
-    <OptionsForm options={props.offering.options} />
-  </AccordionCard>
-);
+export const FormAdditionalConfigurationStep = (props: FormStepProps) => {
+  const customer = useSelector(formCustomerSelector);
+  return (
+    <AccordionCard
+      title={props.title}
+      id={props.id}
+      className="step-card"
+      defaultOpen
+    >
+      <OptionsForm options={props.offering.options} customer={customer} />
+    </AccordionCard>
+  );
+};
--- src/support/api.ts
+++ src/support/api.ts
@@ -1,6 +1,8 @@
 import { ENV } from '@waldur/configs/default';
 import { get, getSelectData } from '@waldur/core/api';
 import { returnReactSelectAsyncPaginateObject } from '@waldur/core/utils';
+import { getStates } from '@waldur/marketplace/resources/list/ResourceStateFilter';
+import { INSTANCE_TYPE, TENANT_TYPE } from '@waldur/openstack/constants';
 
 import { SupportStatistics } from './types';
 
@@ -9,24 +11,34 @@ const tenantSerializer = ({ name, backend_id, project_name }) => ({
   value: `Tenant UUID: ${backend_id}. Name: ${name}`,
 });
 
-export const fetchTenantOptions = async (
+export const fetchOpenstackOptions = async (
   query,
+  type,
   prevOptions,
   currentPage: number,
   customerId,
 ) => {
-  let response = await getSelectData('/openstack-tenants/', {
+  let response = await getSelectData('/marketplace-resources/', {
     field: ['name', 'backend_id', 'project_name'],
     customer_uuid: customerId,
     name: query,
     o: ['project_name', 'name'],
     page: currentPage,
     page_size: ENV.pageSize,
+    state: getStates().map((state) => state.value),
+    offering_type: type,
   });
   response = {
     ...response,
-    options: response.options.map(tenantSerializer),
+    options: response.options.map(
+      type === TENANT_TYPE
+        ? tenantSerializer
+        : type === INSTANCE_TYPE
+          ? instanceSerializer
+          : (o) => o,
+    ),
   };
+
   return returnReactSelectAsyncPaginateObject(
     response,
     prevOptions,
@@ -38,31 +50,6 @@ const instanceSerializer = ({ name, backend_id, project_name }) => ({
   name: `${project_name} / ${name}`,
   value: `Instance UUID: ${backend_id}. Name: ${name}`,
 });
-
-export const fetchInstanceOptions = async (
-  query,
-  prevOptions,
-  currentPage: number,
-  customerId,
-) => {
-  let response = await getSelectData('/openstack-instances/', {
-    field: ['name', 'backend_id', 'project_name'],
-    customer_uuid: customerId,
-    name: query,
-    o: ['project_name', 'name'],
-    page: currentPage,
-    page_size: ENV.pageSize,
-  });
-  response = {
-    ...response,
-    options: response.options.map(instanceSerializer),
-  };
-  return returnReactSelectAsyncPaginateObject(
-    response,
-    prevOptions,
-    currentPage,
-  );
-};
 
 export const getSupportStatistics = () =>
   get<SupportStatistics>(`/support-statistics/`).then(
