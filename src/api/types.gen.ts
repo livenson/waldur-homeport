389afb3c1fa2168c169fd6c2403ee38d273972ef
Viktor Mirieiev <victor.mireyev@gmail.com>
2025-02-28 21:07:58 +0000

    Update Waldur TypeScript SDK

--- src/api/types.gen.ts
+++ src/api/types.gen.ts
@@ -35202,5 +35202,5 @@ export type VmwareVirtualMachineWebConsoleRetrieveResponses = {
 export type VmwareVirtualMachineWebConsoleRetrieveResponse = VmwareVirtualMachineWebConsoleRetrieveResponses[keyof VmwareVirtualMachineWebConsoleRetrieveResponses];
 
 export type ClientOptions = {
-    baseUrl: `${string}://schema.yaml` | (string & {});
+    baseUrl: `${string}://waldur-openapi-schema.yaml` | (string & {});
 };
