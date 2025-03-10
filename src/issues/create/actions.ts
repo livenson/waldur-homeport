dad560012ef7bb5711052e0354da2a08a39fd812
Andrey Izman <andreyizman9@gmail.com>
2025-03-10 19:49:17 +0000

    [WAL-8331] Prevent issue creation modal from closing when outside is clicked if the form is dirty

--- src/issues/create/actions.ts
+++ src/issues/create/actions.ts
@@ -1,6 +1,8 @@
 import { lazyComponent } from '@waldur/core/lazyComponent';
 import { openModalDialog } from '@waldur/modal/actions';
 
+import { ISSUE_CREATION_FORM_ID } from './constants';
+
 const IssueCreateDialog = lazyComponent(() =>
   import('@waldur/issues/create/IssueCreateDialog').then((module) => ({
     default: module.IssueCreateDialog,
@@ -11,4 +13,5 @@ export const openIssueCreateDialog = (resolve) =>
   openModalDialog(IssueCreateDialog, {
     resolve,
     dialogClassName: 'modal-dialog-centered mw-650px',
+    formId: ISSUE_CREATION_FORM_ID,
   });
