2f1456095464c361d090f5069ffd36aa561009f2
Emil Ojagverdiyev <ojagverdiyev13@gmail.com>
2025-03-24 10:19:23 +0000

    [WAL-8126] Add filters to user affiliations table

--- src/user/affiliations/UserAffiliationsList.tsx
+++ src/user/affiliations/UserAffiliationsList.tsx
@@ -1,4 +1,7 @@
 import { FunctionComponent, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { getFormValues } from 'redux-form';
+import { UserPermissionsListData } from 'waldur-js-client';
 
 import { formatDate } from '@waldur/core/dateUtils';
 import { Link } from '@waldur/core/Link';
@@ -10,16 +13,53 @@ import Table from '@waldur/table/Table';
 import { useTable } from '@waldur/table/useTable';
 
 import { RolePopover } from './RolePopover';
+import { UserAffiliationsFilter } from './UserAffiliationsFilter';
 
 interface UserAffiliationsListProps {
   user;
   hasActionBar?: boolean;
 }
 
+interface UserAffiliationsFilterValues {
+  scope_type: string;
+  scope_name: string;
+  role: {
+    uuid: string;
+  };
+}
+
 export const UserAffiliationsList: FunctionComponent<
   UserAffiliationsListProps
 > = ({ user, hasActionBar = true }) => {
-  const filter = useMemo(() => ({ user: user.uuid }), [user]);
+  const formValues = (useSelector((state) =>
+    getFormValues('UserAffiliationsFilter')(state),
+  ) as UserAffiliationsFilterValues) || {
+    scope_type: undefined,
+    scope_name: undefined,
+    role: undefined,
+  };
+  const filter = useMemo(() => {
+    const result: UserPermissionsListData['query'] = {
+      user: user.uuid,
+    };
+
+    if (formValues?.scope_type) {
+      result.scope_type = formValues.scope_type;
+    }
+    if (formValues?.scope_name) {
+      result.scope_name = formValues.scope_name;
+    }
+    if (formValues?.role) {
+      result.role_uuid = formValues.role.uuid;
+    }
+
+    return result;
+  }, [
+    user.uuid,
+    formValues.scope_type,
+    formValues.scope_name,
+    formValues.role,
+  ]);
   const props = useTable({
     table: 'UserAffiliationsList',
     fetchData: createFetcher('user-permissions'),
@@ -31,6 +71,7 @@ export const UserAffiliationsList: FunctionComponent<
     {
       title: translate('Scope type'),
       render: ({ row }) => <>{formatRoleType(row.scope_type)}</>,
+      filter: 'scope_type',
     },
     {
       title: translate('Scope name'),
@@ -44,6 +85,7 @@ export const UserAffiliationsList: FunctionComponent<
         ) : (
           <>{row.scope_name}</>
         ),
+      filter: 'scope_name',
     },
     {
       title: translate('Organization'),
@@ -67,6 +109,7 @@ export const UserAffiliationsList: FunctionComponent<
     {
       title: translate('Role name'),
       render: ({ row }) => <RolePopover roleName={row.role_name} />,
+      filter: 'role',
     },
     {
       title: translate('Valid till'),
@@ -86,6 +129,7 @@ export const UserAffiliationsList: FunctionComponent<
       columns={columns}
       verboseName={translate('affiliations')}
       title={translate('Roles and permissions')}
+      filters={<UserAffiliationsFilter />}
       initialPageSize={100}
       hasActionBar={hasActionBar}
     />
