1bcdadce525a73446690dad068e318cb76e8095e
Viktor Mirieiev <victor.mireyev@gmail.com>
2025-04-24 18:47:07 +0000

    Allow YAML import only to staff [WAL-8539]

--- src/rancher/cluster/ImportYAMLButton.tsx
+++ src/rancher/cluster/ImportYAMLButton.tsx
@@ -6,6 +6,7 @@ import { lazyComponent } from '@waldur/core/lazyComponent';
 import { translate } from '@waldur/i18n';
 import { openModalDialog } from '@waldur/modal/actions';
 import { ActionButton } from '@waldur/table/ActionButton';
+import { useUser } from '@waldur/workspace/hooks';
 
 const ImportYAMLDialog = lazyComponent(() =>
   import('./ImportYAMLDialog').then((module) => ({
@@ -13,10 +14,14 @@ const ImportYAMLDialog = lazyComponent(() =>
   })),
 );
 
-export const ImportYAMLButton: FunctionComponent<{ cluster_id }> = ({
+export const ImportYAMLButton: FunctionComponent<{ cluster_id: string }> = ({
   cluster_id,
 }) => {
   const dispatch = useDispatch();
+  const user = useUser();
+  if (!user.is_staff) {
+    return null;
+  }
   return (
     <ActionButton
       title={translate('Import YAML')}
