45fa7c73c341eb03b338e166f2e08e7f7691cc4c
Andrey Izman <andreyizman9@gmail.com>
2025-05-12 09:39:59 +0000

    [WAL-8626] Wrap long descriptions into a collapse/expandable view and fixed x-space between columns

--- src/marketplace/offerings/update/DefaultOfferingEditPanel.tsx
+++ src/marketplace/offerings/update/DefaultOfferingEditPanel.tsx
@@ -1,13 +1,15 @@
 import { get } from 'lodash-es';
 
 import { CheckOrX } from '@waldur/core/CheckOrX';
-import { SecretField } from '@waldur/form';
+import { TruncatedDescription } from '@waldur/core/TruncatedDescription';
+import { SecretField, TextField } from '@waldur/form';
 import { AwesomeCheckboxField } from '@waldur/form/AwesomeCheckboxField';
 import { CommaSeparatedListField } from '@waldur/form/CommaSeparatedListField';
 import FormTable from '@waldur/form/FormTable';
 import { SecretField as PlainSecretField } from '@waldur/marketplace/common/SecretField';
 import { FieldEditButton } from '@waldur/marketplace/offerings/update/integration/FieldEditButton';
 import { OfferingEditPanelFormProps } from '@waldur/marketplace/offerings/update/integration/types';
+import { DASH_ESCAPE_CODE } from '@waldur/table/constants';
 
 export interface OfferingEditField {
   key: string;
@@ -33,6 +35,10 @@ export const DefaultOfferingEditPanel = (
         value={
           field.value ? (
             field.value(get(props.offering, field.key))
+          ) : field.component === TextField ? (
+            <TruncatedDescription
+              text={get(props.offering, field.key) || DASH_ESCAPE_CODE}
+            />
           ) : field.component === SecretField ? (
             <PlainSecretField value={get(props.offering, field.key)} />
           ) : field.component === AwesomeCheckboxField ? (
