95873bf9b253504878c8974cc617e514b047ab0d
Emil Ojagverdiyev <ojagverdiyev13@gmail.com>
2025-06-17 08:35:43 +0000

    [WAL-8413] Use same pencilsimpleicon for consistency

--- src/form/EditAction.tsx
+++ src/form/EditAction.tsx
@@ -1,4 +1,4 @@
-import { PencilIcon } from '@phosphor-icons/react';
+import { PencilSimpleIcon } from '@phosphor-icons/react';
 
 import { translate } from '@waldur/i18n';
 import { ActionItem } from '@waldur/resource/actions/ActionItem';
@@ -7,7 +7,7 @@ export const EditAction = (props) => {
   return (
     <ActionItem
       title={translate('Edit')}
-      iconNode={<PencilIcon weight="bold" />}
+      iconNode={<PencilSimpleIcon weight="bold" />}
       {...props}
     />
   );
